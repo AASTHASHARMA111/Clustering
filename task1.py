aefd752fe75063900ece642f6ae1797e5a8922f2
ASTHASHARMA111 <saasthas111@gmail.com>
2024-11-04 21:05:29 +0530

    file path

--- task1.py
+++ task1.py
@@ -4,3 +4,5 @@ from sklearn.cluster import KMeans
 import numpy as np
 
 # Load the data from the Excel files
+train_file_path = '/content/drive/MyDrive/ResoluteAI/Aastha Sharma Task 1/train.xlsx'
+test_file_path = '/content/drive/MyDrive/ResoluteAI/Aastha Sharma Task 1/test.xlsx
