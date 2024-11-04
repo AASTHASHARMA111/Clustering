4af6e23324e92e47e9d0fc731d14605afcd6e231
AASTHASHARMA111 <saasthas111@gmail.com>
2024-11-04 21:07:35 +0530

    read data

--- task1.py
+++ task1.py
@@ -6,3 +6,7 @@ import numpy as np
 # Load the data from the Excel files
 train_file_path = '/content/drive/MyDrive/ResoluteAI/Aastha Sharma Task 1/train.xlsx'
 test_file_path = '/content/drive/MyDrive/ResoluteAI/Aastha Sharma Task 1/test.xlsx
+
+# Read the train and test datasets
+train_data = pd.read_excel(train_file_path)
+test_data = pd.read_excel(test_file_path)
